9598f5971051ac9c8f25ae15c6b6399e36bdd303
davinahouquet <davina.houquet@hotmail.com>
2023-04-28 15:37:46 +0200

    Affichage liste des potions et nb d'ingrédients

--- index.php
+++ index.php
@@ -51,9 +51,9 @@ echo "Exercice Gaulois PDO<br><br>1. Afficher la liste des gaulois avec nom du p
 
     $specaliteGaulois = $db->prepare($sqlQuery2);
     $specaliteGaulois->execute();
-    $gaulois1 = $specaliteGaulois->fetchAll();
+    $gaulois2 = $specaliteGaulois->fetchAll();
 
-    foreach($gaulois1 as $specialite){
+    foreach($gaulois2 as $specialite){
         echo "<table cellpadding=2 cellspacing=2>
                 <td>".$specialite['nom_specialite']."</td>
                 <td>".$specialite['nb_persos']."</td>
@@ -65,6 +65,20 @@ echo "Exercice Gaulois PDO<br><br>1. Afficher la liste des gaulois avec nom du p
     echo "<br>3. Afficher la liste des potions : nom de la potion et nombre d'ingrédients de la potion<br><br>";
 
     $sqlQuery3 = 
-        "SELECT ";
+        "SELECT potion.nom_potion, COUNT(ingredient.id_ingredient) AS 'nb_ingredients' FROM potion
+        INNER JOIN composer ON potion.id_potion = composer.id_potion
+        INNER JOIN ingredient ON composer.id_ingredient = ingredient.id_ingredient
+        GROUP BY potion.id_potion
+        ORDER BY COUNT('nb_ingredients')";
+ 
+    $potionGaulois = $db->prepare($sqlQuery3);
+    $potionGaulois->execute();
+    $gaulois3 = $potionGaulois->fetchAll();
 
+    foreach($gaulois3 as $potion){
+        echo "<table cellpadding=2 cellspacing=2>
+                <td>".$potion['nom_potion']."</td>
+                <td>".$potion['nb_ingredients']."</td>
+            </table>";
+    }
 ?>
