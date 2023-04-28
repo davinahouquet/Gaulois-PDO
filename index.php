f2e56a0c91af8deaef568685e9ce723828e187e8
davinahouquet <davina.houquet@hotmail.com>
2023-04-28 14:26:36 +0200

    Affichage spécialité et lieu d'habitation

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 <?php
 
-    echo "Exercice Gaulois PDO<br>
-    <br>
-    Afficher (dans des tableaux HTML): <br>
-    <br>
-    -- la liste des gaulois avec nom du personnage, spécialité et lieu d'habitation (de la table lieu)
-    <br>
-    -- la liste des spécialités : nom de la spécialité et nombre de gaulois par spécialité 
-    <br>
-    -- la liste des potions : nom de la potion et nombre d'ingrédients de la potion
-    <br>";
+    echo "Exercice Gaulois PDO<br><br>1.Afficher la liste des gaulois avec nom du personnage, spécialité et lieu d'habitation (de la table lieu)<br><br>";
 
 try{
     $db = new PDO(
-        'mysql:host=127.0.0.1;dbname=gaulois;charset=utf8',
+        'mysql:host=localhost;dbname=gaulois;charset=utf8',
         'root',
         '',
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
@@ -23,14 +14,28 @@ catch (Exception $e){
     die('Erreur : ' .$e->getMessage());
 }
 
-$sqlQuery = "SELECT nom_personnage FROM personnage";
+$sqlQuery1 = 
+    "SELECT nom_personnage, specialite.nom_specialite, lieu.nom_lieu FROM personnage
+    INNER JOIN lieu ON personnage.id_lieu = lieu.id_lieu
+    INNER JOIN specialite ON personnage.id_specialite = specialite.id_specialite";
 
-$personnageGaulois = $db->prepare($sqlQuery);
+$personnageGaulois = $db->prepare($sqlQuery1);
 $personnageGaulois->execute();
 $gaulois = $personnageGaulois->fetchAll();
 
+    echo "<table cellpadding=2 cellspacing=2 style='text-align : center'>
+            <tr>
+                <td>Nom</td>
+                <td>Lieu</td>
+                <td>Specialite</td>
+            </tr>";
+
 foreach($gaulois as $perso){
-    echo "<p>".$perso['nom_personnage']."</p>";
+    echo  "<table cellpadding=2 cellspacing=2>
+            <td>".$perso['nom_personnage']."</td>
+            <td>".$perso['nom_lieu']."</td>
+            <td>".$perso['nom_specialite']."</td>
+          </table>";
 }
 
 ?>
